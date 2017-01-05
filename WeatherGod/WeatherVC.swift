aee3d5fd0d239f22e819f2954ee3e3ee8476f1de
Roydon Jeffrey <brushdam14823@gmail.com>
2017-01-04 20:02:33 -0500

    Struggling through the auto-layout setup with stackViews and added a tableView, designed the prototype cell with auto-layout and stackViews, connected the IBOutlets and inherited the tableView Delegate and DataSource along with the required methods

---
+++ WeatherGod/WeatherVC.swift
@@ -0,0 +1,49 @@
+//
+//  WeatherVC.swift
+//  WeatherGod
+//
+//  Created by Roydon Jeffrey on 1/3/17.
+//  Copyright © 2017 Italyte. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var tempLbl: UILabel!
+    @IBOutlet weak var cityLbl: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherTypeLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+}
+
