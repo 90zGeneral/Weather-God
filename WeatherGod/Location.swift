61424f4d95339101d470faa224643b3200d9f071
Roydon Jeffrey <brushdam14823@gmail.com>
2017-01-09 17:09:58 -0500

    Added a helpful comment for my learning purposes

--- WeatherGod/Location.swift
+++ WeatherGod/Location.swift
@@ -14,6 +14,7 @@ class Location {
     //Singleton to pass data to the properties of this class
     static var sharedInstance = Location()
     
+    //The initializer MUST be private to be a true singleton. A single instance of this class should exist at all times throughout this app
     private init() {}
     
     //Coordinates
