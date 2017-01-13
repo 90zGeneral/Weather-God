0821cf9567b422518d88e3a9e651d0569e65a33b
Roydon Jeffrey <brushdam14823@gmail.com>
2017-01-13 12:09:12 -0500

    Added another viewController to display the sunrise, sunset, and weather description for the current city and made the json parsing to extract those values in their correct formats

--- WeatherGod/CurrentWeather.swift
+++ WeatherGod/CurrentWeather.swift
@@ -13,7 +13,10 @@ class CurrentWeather {
     private var _cityName: String!
     private var _date: String!
     private var _weatherType: String!
+    private var _weatherDescription: String!
     private var _currentTemp: Double!
+    private var _sunrise: String!
+    private var _sunset: String!
     
     //Getters
     var cityName: String {
@@ -48,12 +51,36 @@ class CurrentWeather {
         return _weatherType
     }
     
+    var weatherDescription: String {
+        if _weatherDescription == nil {
+            _weatherDescription = ""
+        }
+        
+        return _weatherDescription
+    }
+    
     var currentTemp: Double {
         if _currentTemp == nil {
             _currentTemp = 0.0
         }
         
         return _currentTemp
+    }
+    
+    var sunrise: String {
+        if _sunrise == nil {
+            _sunrise = ""
+        }
+        
+        return _sunrise
+    }
+    
+    var sunset: String {
+        if _sunset == nil {
+            _sunset = ""
+        }
+        
+        return _sunset
     }
     
     
@@ -79,14 +106,22 @@ class CurrentWeather {
                 }
                 
                 //To get the weather type
-                if let weatherArr = dict["weather"] as? [[String: Any]] {
-                    if let main = weatherArr[0]["main"] as? String {
+                if let weatherArr1 = dict["weather"] as? [[String: Any]] {
+                    if let main = weatherArr1[0]["main"] as? String {
                         self._weatherType = main.capitalized
                         print(self._weatherType)
                     }
                 }
                 
-                //to get the current temperature
+                //To get the weather description
+                if let weatherArr2 = dict["weather"] as? [[String: Any]] {
+                    if let main = weatherArr2[0]["description"] as? String {
+                        self._weatherDescription = main.capitalized
+                        print(self._weatherDescription)
+                    }
+                }
+                
+                //To get the current temperature
                 if let dictMain = dict["main"] as? [String: Any] {
                     if let temp = dictMain["temp"] as? Double {
                         
@@ -97,6 +132,32 @@ class CurrentWeather {
                         print(self._currentTemp)
                     }
                 }
+                
+                //To get the sunrise time
+                if let sys1 = dict["sys"] as? [String: Any] {
+                    if let sunrise = sys1["sunrise"] as? Double {
+                        
+                        //The date is in a Unix standard that needs to be converted
+                        let unixConvertedDate = Date(timeIntervalSince1970: sunrise)
+                        
+                        //Call the method defined in the Date extnsion on unixConvertedDate
+                        self._sunrise = unixConvertedDate.timeOfTheDay()
+                        print(self._sunrise)
+                    }
+                }
+                
+                //To get the sunset time
+                if let sys2 = dict["sys"] as? [String: Any] {
+                    if let sunset = sys2["sunset"] as? Double {
+                        
+                        //The date is in a Unix standard that needs to be converted
+                        let unixConvertedDate = Date(timeIntervalSince1970: sunset)
+                        
+                        //Call the method defined in the Date extnsion on unixConvertedDate
+                        self._sunset = unixConvertedDate.timeOfTheDay()
+                        print(self._sunset)
+                    }
+                }
             }
             //Tell it to complete
             completed()
@@ -104,3 +165,19 @@ class CurrentWeather {
     }
     
 }
+
+
+extension Date {
+    
+    //Find the time of the day
+    func timeOfTheDay() -> String {
+        
+        //A date formatter to determine its presentation
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .none      //Omit the date
+        dateFormatter.timeStyle = .short      //Get the short timestamp for the day
+        
+        return dateFormatter.string(from: self)
+    }
+}
+
