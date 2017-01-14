56e0fc6d5345e88f99b52b8a7499d9ed103b1f88
Roydon Jeffrey <brushdam14823@gmail.com>
2017-01-13 20:48:50 -0500

    Updated the UI for the DetailsVC and display the current city name in the navigation bar. I think the design can be a little better and the search feature is not yet ready.

--- WeatherGod/DetailsVC.swift
+++ WeatherGod/DetailsVC.swift
@@ -9,11 +9,46 @@
 import UIKit
 
 class DetailsVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var sunrise: UILabel!
+    @IBOutlet weak var sunset: UILabel!
+    @IBOutlet weak var weatherDescription: UILabel!
+    @IBOutlet weak var humidity: UILabel!
+    @IBOutlet weak var windSpeed: UILabel!
+    @IBOutlet weak var navBar: UINavigationBar!
+    
+    
+    //Declare a new instance of CurrentWeather
+    var currentWeather: CurrentWeather!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //Instantiate the new instance
+        currentWeather = CurrentWeather()
+        
+        currentWeather.downloadWeatherDetails {
+            //Function call
+            self.updateUI()
+        }
+    }
+    
+    //Update the view
+    func updateUI() {
+        
+        sunrise.text = currentWeather.sunrise + " EST"
+        sunset.text = currentWeather.sunset + " EST"
+        weatherDescription.text = currentWeather.weatherDescription
+        humidity.text = "\(currentWeather.humidity)%"
+        windSpeed.text = "\(Int(round(currentWeather.windSpeed))) mph"
+        
+        //Let the Nav bar display the current city
+        navBar.topItem?.title = currentWeather.cityName.capitalized
+        
+        
     }
 
 }
