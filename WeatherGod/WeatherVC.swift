e93012969a217dced39fda8ae799edc560fb628e
Roydon Jeffrey <brushdam14823@gmail.com>
2017-01-14 15:54:53 -0500

    Very minor change to one of the sky's background color

--- WeatherGod/WeatherVC.swift
+++ WeatherGod/WeatherVC.swift
@@ -141,7 +141,7 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource, C
             weatherImage.image = UIImage(named: currentWeather.weatherType)
         }
         
-        //To set The Sky's background depending on the weather type
+        //To set The Sky's background depending on the weather type. Use http://uicolor.xyz/#/hex-to-ui to convert hex color
         if weatherTypeLbl.text == "Clear" {
             theSky.backgroundColor = UIColor(red: 0.00, green: 0.60, blue: 1.00, alpha: 1.0)
             
@@ -149,7 +149,7 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource, C
             theSky.backgroundColor = UIColor(red: 0.70, green: 0.70, blue: 0.70, alpha: 1.0)
             
         }else if weatherTypeLbl.text == "Clouds" || weatherTypeLbl.text == "Snow" {
-            theSky.backgroundColor = UIColor(red: 0.40, green: 0.40, blue: 0.40, alpha: 1.0)
+            theSky.backgroundColor = UIColor(red: 0.50, green: 0.50, blue: 0.50, alpha: 1.0)
             
         }else if weatherTypeLbl.text == "Thunderstorm" || weatherTypeLbl.text == "Rain" {
             theSky.backgroundColor = UIColor(red: 0.20, green: 0.20, blue: 0.20, alpha: 1.0)
